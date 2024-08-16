db972313f496a4951df0688f6142a62f41eb1e2e
fan windows 7/xp <98217346+vendettathevhsdvr@users.noreply.github.com>
2024-08-16 20:43:06 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord
+
+from bot_logic import gen_pass
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hi!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\\U0001f642")
+    elif message.content.startswith('$pass'):
+        await message.channel.send(gen_pass(10))
+    else:
+        await message.channel.send(message.content)
+
+client.run('token')
